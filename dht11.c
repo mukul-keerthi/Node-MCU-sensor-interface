ca1e08ccbc2b87c698828b8fc71f7410d4388e41
mkl45 <mukul04.sk@gmail.com>
2019-03-28 11:10:42 +0000

    Create dht11.c
    DHT 11 sensor senses temperarture and humidity through nodeMCU Wifi board using Blynk platform.

---
+++ dht11.c
@@ -0,0 +1,79 @@
+**************************************************************
+ *Author: Mukul
+*Date 08/02/2019 
+
+*This example shows how value can be pushed from Arduino to
+ * the Blynk App.
+ *
+ * WARNING :
+ * For this example you'll need SimpleTimer library:
+ *   https://github.com/jfturcot/SimpleTimer
+ * and Adafruit DHT sensor library:
+ *   https://github.com/adafruit/DHT-sensor-library
+ *
+ * App project setup:
+ *   Value Display widget attached to V5
+ *   Value Display widget attached to V6
+ *
+ **************************************************************/
+
+#define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
+#include <SPI.h>
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <SimpleTimer.h>
+#include <DHT.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "958fa2f6e1c24ce19bb13c4394ce3686";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "ulwireless";
+char pass[] = "";
+
+#define DHTPIN 2          // What digital pin we're connected to
+
+// Uncomment whatever type you're using!
+#define DHTTYPE DHT11     // DHT 11
+//#define DHTTYPE DHT22   // DHT 22, AM2302, AM2321
+//#define DHTTYPE DHT21   // DHT 21, AM2301
+
+DHT dht(DHTPIN, DHTTYPE);
+SimpleTimer timer;
+
+// This function sends Arduino's up time every second to Virtual Pin (5).
+// In the app, Widget's reading frequency should be set to PUSH. This means
+// that you define how often to send data to Blynk App.
+void sendSensor()
+{
+  float h = dht.readHumidity();
+  float t = dht.readTemperature(); // or dht.readTemperature(true) for Fahrenheit
+
+  if (isnan(h) || isnan(t)) {  //If no readings is acquired from temp and humidity sernsor
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  // Can send any value at any time.
+  // don't send more that 10 values per second.
+  Blynk.virtualWrite(V5, h);
+  Blynk.virtualWrite(V6, t);
+}
+
+void setup()
+{
+  Serial.begin(9600); // See the connection status in Serial Monitor
+  Blynk.begin(auth, ssid, pass);
+
+  dht.begin();
+
+  // Setup a function to be called every second
+  timer.setInterval(1000L, sendSensor);
+}
+
+void loop()
+{
+  Blynk.run(); // Initiates Blynk
+  timer.run(); // Initiates SimpleTimer
+}
